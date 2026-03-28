d85255cf800b9800a17c33c432d73bb6382218c5
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-03-28 16:16:35 -0500

    Se creo CRUD visits

---
+++ app/Filament/Resources/Visits/VisitsResource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\Visits;
+
+use App\Filament\Resources\Visits\Pages\CreateVisits;
+use App\Filament\Resources\Visits\Pages\EditVisits;
+use App\Filament\Resources\Visits\Pages\ListVisits;
+use App\Filament\Resources\Visits\Schemas\VisitsForm;
+use App\Filament\Resources\Visits\Tables\VisitsTable;
+use App\Models\Visits;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class VisitsResource extends Resource
+{
+    protected static ?string $model = Visits::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'id';
+
+    public static function form(Schema $schema): Schema
+    {
+        return VisitsForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return VisitsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListVisits::route('/'),
+            'create' => CreateVisits::route('/create'),
+            'edit' => EditVisits::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function visits()
+    {
+        return $this->hasMany(visits::class);
+    }
 }
