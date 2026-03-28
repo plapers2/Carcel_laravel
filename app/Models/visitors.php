c95fc36f69dfeaa8240be294d3ffcba022165f41
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-03-27 21:31:58 -0500

    Se crea y configura modelo visitors

---
+++ app/Models/visitors.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class visitors extends Model
+{
+    protected $fillable = ['name', 'identification_number'];
+    public function visits()
+    {
+        return $this->belongsTo(Visit::class);
+    }
+}
