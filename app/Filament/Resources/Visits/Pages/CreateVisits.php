3a72c1ebd72b5971d630acac921aec6e5d447ab0
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-03-30 13:02:13 -0500

    Se cambian nombres de funciones a ingles en createVisits

--- app/Filament/Resources/Visits/Pages/CreateVisits.php
+++ app/Filament/Resources/Visits/Pages/CreateVisits.php
@@ -9,45 +9,45 @@ class CreateVisits extends CreateRecord
 {
     protected static string $resource = VisitsResource::class;
     // Hook de Filament
-    protected function mutateFormDataBeforeCreate(array $data): array
+    private function validateSunday(array &$data): void
     {
-        $this->validarDomingo($data['start_date']);
-        $this->validarFecha($data);
-        $this->asignarGuardia($data);
-        $this->asignarVerificacion($data);
-
-        return $data;
-    }
-    private function validarDomingo(string $date): void
-    {
-        $date = \Carbon\Carbon::parse($date);
+        $date = \Carbon\Carbon::parse($data['start_date']);
 
         if ($date->dayOfWeek !== \Carbon\Carbon::SUNDAY) {
             \Filament\Notifications\Notification::make()
                 ->title('Error')
-                ->body('Las visitas solo se permiten los domingos.')
+                ->body('Visits are only allowed on Sundays!')
                 ->danger()
                 ->send();
             $this->halt();
         }
     }
-    private function asignarGuardia(array &$data): void
+    private function assignGuard(array &$data): void
     {
         $data['users_id'] = auth()->id();
     }
-    private function asignarVerificacion(array &$data): void
+    private function assignVerification(array &$data): void
     {
         $data['verification'] = 'En curso';
     }
-    private function validarFecha(array &$data): void
+    private function validateFutureDate(array &$data): void
     {
-        if ($data['start_date'] < now()) {
+        if (\Carbon\Carbon::parse($data['start_date'])->isPast()) {
             \Filament\Notifications\Notification::make()
                 ->title('Error')
-                ->body('La fecha debe ser mayor a la actual!.')
+                ->body('The date must be later than the current date!')
                 ->danger()
                 ->send();
             $this->halt();
         }
+    }
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $this->validateSunday($data['start_date']);
+        $this->validateFutureDate($data);
+        $this->assignGuard($data);
+        $this->assignVerification($data);
+
+        return $data;
     }
 }
